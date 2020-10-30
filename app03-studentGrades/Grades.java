911c21e0485e714228f5fdaa7258421c4e40f652
Alex Clements <71945393+clements1992@users.noreply.github.com>
2020-10-30 15:21:25 +0000

    Completed grades

---
+++ app03-studentGrades/Grades.java
@@ -0,0 +1,11 @@
+
+/**
+ * Enumeration class Grades - write a description of the enum class here
+ *
+ * @author Alex Clements
+ * @version 30/10/2020
+ */
+public enum Grades
+{
+    X, F, D, C, B, A
+}
