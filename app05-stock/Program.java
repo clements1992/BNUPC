30aae029e97f8e649b36ee06b6b4a1e2ac686bd2
Alex Clements <71945393+clements1992@users.noreply.github.com>
2020-11-26 12:25:42 +0000

    author/date changed

--- app05-stock/Program.java
+++ app05-stock/Program.java
@@ -2,16 +2,15 @@
 /**
  * Write a description of class Program here.
  *
- * @author (your name)
- * @version (a version number or a date)
- */
+ * @author Alex Clements
+ * @version 13/11/2020
+ Because this is static you can call it straight from the class w/out having to make an object*/
 public class Program
 {
     private static StockApp app;
 
     /**
-     * This class creates and runs an instance of
-     * the StockApp class
+     * This create a new app called stock and runs it
      */
     public static void main()
     {
